68a05dd8ddf0a4ffcecbf4bcbeb8f73a8530347d
ABC <abc@gmail.com>
2023-12-18 16:48:58 +0530

    Updated code

--- start-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-aws s3 cp s3://kiko-deployment-assests/dev-env/.env /home/ubuntu/kiko-backend/
-#docker-compose -f /home/ubuntu/kiko-backend/docker-compose.yml down
-docker-compose -f /home/ubuntu/kiko-backend/docker-compose.yml up -d --build
